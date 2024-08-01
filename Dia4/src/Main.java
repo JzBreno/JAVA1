c3a9b795e7ffe409af8ef64fa270720e00ca2d31
JzBreno <jose.breno.sousa61@aluno.ifce.edu.br>
2024-08-01 16:55:05 -0300

    Loops

--- Dia4/src/Main.java
+++ Dia4/src/Main.java
@@ -3,6 +3,27 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("printando");
-
+        HelloWorld();
     }
+
+    //metodos de classe
+    public static void ForLoop1(){
+
+        for(int i=0;i<=100;i+=2){
+                System.out.println(i);
+        }
+    }
+
+    public static void HelloWorld(){
+        String frase = "Hello World";
+        char[] chars = frase.toCharArray();
+        for(char c : chars){
+            System.out.print(c+" ");
+        }
+    }
+
 }
+
+
+
+
