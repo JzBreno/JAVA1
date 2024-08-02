bec51a5bcec15c965345bd48375d7d9434d660c1
JzBreno <jose.breno.sousa61@aluno.ifce.edu.br>
2024-08-02 20:08:11 -0300

    arquivos .jar e metodos de classe

---
+++ dia5/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+    //chamaremos nossos metodos no nosso main com retorno void
+        int soma = somar(3,9);
+        System.out.println(soma);
+    }
+    //exeplos de metodos
+    public static int somar(int a,int b){
+        return a + b;
+    }
+
+
+
+}
